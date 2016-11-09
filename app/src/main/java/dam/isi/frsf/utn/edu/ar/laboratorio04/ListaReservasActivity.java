bb2960af486ece05196f34204f2739c174cc6831
martin2217 <martin_f1g@hotmail.com>
2016-11-08 21:03:04 -0300

    Punto 4 completo .. LongClick para reservar un departamento

--- app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio04/ListaReservasActivity.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio04/ListaReservasActivity.java
@@ -7,16 +7,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import dam.isi.frsf.utn.edu.ar.laboratorio04.modelo.Reserva;
-import dam.isi.frsf.utn.edu.ar.laboratorio04.utils.BuscarReservasTask;
-import dam.isi.frsf.utn.edu.ar.laboratorio04.utils.BusquedaFinalizadaListener;
-import dam.isi.frsf.utn.edu.ar.laboratorio04.utils.FormBusqueda;
 
 public class ListaReservasActivity extends AppCompatActivity {
 
@@ -51,5 +50,7 @@ public class ListaReservasActivity extends AppCompatActivity {
         lista=MainActivity.getReservas();
         reservasAdapter = new ReservaAdapter(ListaReservasActivity.this,lista);
         listaReservas.setAdapter(reservasAdapter);
+
     }
+
 }
